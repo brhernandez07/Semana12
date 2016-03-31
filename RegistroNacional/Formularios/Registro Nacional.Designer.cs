c4cdca2c9670594ff8af2780fb0bd06a5a3e35eb
dj.bryan.h@gmail.com <dj.bryan.h@gmail.com>
2016-03-30 20:44:50 -0600

    h

--- RegistroNacional/Formularios/Registro Nacional.Designer.cs
+++ RegistroNacional/Formularios/Registro Nacional.Designer.cs
@@ -51,6 +51,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.btn_GenerarPlaca = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // lblCedula
@@ -238,11 +239,20 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(193, 257);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 20;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 267);
+            this.ClientSize = new System.Drawing.Size(432, 335);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_GenerarPlaca);
             this.Controls.Add(this.txt_anoVehiculo);
@@ -294,6 +304,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btn_GenerarPlaca;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
